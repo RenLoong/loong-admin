1cce68ffd06f5514bf0cf5891c79c3c89a35e255
Renloong <unknown.renlong@gmail.com>
2025-07-07 14:35:36 +0800

    toTree

--- app/expose/utils/DataModel.php
+++ app/expose/utils/DataModel.php
@@ -78,4 +78,31 @@ class DataModel implements ArrayAccess, JsonSerializable
     {
         return $this->data;
     }
+    private function _organizeRecords($regions,$idKey)
+    {
+        $organizedRegions = [];
+        foreach ($regions as $region) {
+            $organizedRegions[$region[$idKey]] = $region;
+            $organizedRegions[$region[$idKey]]['children'] = [];
+        }
+        return $organizedRegions;
+    }
+    private function _buildTree($organized,$pidKey)
+    {
+        $tree = [];
+        foreach ($organized as $id => $record) {
+            if ($record[$pidKey]) {
+                $organized[$record[$pidKey]]['children'][] = &$organized[$id];
+            } else {
+                $tree[] = &$organized[$id];
+            }
+        }
+        return $tree;
+    }
+    public function toTree($idKey='id',$pidKey='pid')
+    {
+        $organized=$this->_organizeRecords($this->data,$idKey);
+        $data=$this->_buildTree($organized,$pidKey);
+        return $data;
+    }
 }
