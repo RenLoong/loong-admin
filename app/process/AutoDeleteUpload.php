2b2fe8d2862e5ff4a8f802af41c80dba033bc07b
Renloong <unknown.renlong@gmail.com>
2025-07-17 16:21:47 +0800

    AutoDeleteUpload

--- app/process/AutoDeleteUpload.php
+++ app/process/AutoDeleteUpload.php
@@ -4,6 +4,7 @@ namespace app\process;
 
 use app\model\Uploads;
 use Shopwwi\WebmanFilesystem\FilesystemFactory;
+use support\Log;
 use Workerman\Crontab\Crontab;
 class AutoDeleteUpload
 {
@@ -11,13 +12,15 @@ class AutoDeleteUpload
     {
         # 查询冻结超过24小时的订单，解冻
         new Crontab('0 */1 * * * *', function(){
-            print_r('AutoDeleteUpload');
             $models=Uploads::whereNotNull('auto_delete_time')->whereTime('auto_delete_time','<',date('Y-m-d H:i:s'))->select();
             foreach ($models as $model) {
-                print_r($model->path);
-                $filesystem =  FilesystemFactory::get($model->channels);
-                $filesystem->delete($model->path);
-                $model->delete();
+                try {
+                    $filesystem =  FilesystemFactory::get($model->channels);
+                    $filesystem->delete($model->path);
+                    $model->delete();
+                } catch (\Throwable $th) {
+                    Log::error('删除文件"'.$model->path.'"失败：'.$th->getMessage(),$th->getTrace());
+                }
             }
         });
     }
